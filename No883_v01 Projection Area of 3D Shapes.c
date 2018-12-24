a6ec881829fe0f559c1f24a1fe666a38ff9444fb
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-12-24 21:57:12 +0800

    Update No883_v01 Projection Area of 3D Shapes.c

--- No883_v01 Projection Area of 3D Shapes.c
+++ No883_v01 Projection Area of 3D Shapes.c
@@ -1,22 +1,22 @@
 int projectionArea(int** grid, int gridRowSize, int *gridColSizes) {
     int sum = 0;
-    int xBuffer[50] = {0}, yBuffer[50] = {0};
+    int xzHist[50] = {0}, yzHist[50] = {0};
     
     for(int i = 0; i < gridRowSize; ++i){
         for(int j = 0; j < gridRowSize; ++j){
             if(grid[i][j] > 0){
                 ++sum;
             }
-            if(grid[i][j] > yBuffer[j]){
-                yBuffer[j] = grid[i][j];
+            if(grid[i][j] > yzHist[j]){
+                yzHist[j] = grid[i][j];
             }
-            if(grid[i][j] > xBuffer[i]){
-                xBuffer[i] = grid[i][j];
+            if(grid[i][j] > xzHist[i]){
+                xzHist[i] = grid[i][j];
             }
         }    
     }
     for(int i = 0; i < gridRowSize; ++i){
-        sum += xBuffer[i] + yBuffer[i];
+        sum += xzHist[i] + yzHist[i];
     }
     return sum;
     
