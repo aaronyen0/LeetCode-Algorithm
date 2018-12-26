186c64047912be8f50217e9cd0d293e673c35ec5
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-12-27 01:57:16 +0800

    Update No695_v02 Max Area of Island.c

--- No695_v02 Max Area of Island.c
+++ No695_v02 Max Area of Island.c
@@ -38,3 +38,40 @@ int maxAreaOfIsland(int** grid, int gridRowSize, int gridColSize) {
     }
     return maxArea;
 }
+
+
+/*8ms beats 100%，前一個版本傳太多參數了
+int gRow;
+int gCol;
+
+int DFS2(int** grid, int i, int j, int sum){
+    if((i >= 0 && i < gRow) && (j >= 0 && j < gCol) && grid[i][j] == 1){
+        grid[i][j] = 2;
+        sum = DFS2(grid, i - 1, j, sum);
+        sum = DFS2(grid, i + 1, j, sum);
+        sum = DFS2(grid, i, j - 1, sum);
+        sum = DFS2(grid, i, j + 1, sum);
+        return ++sum;
+    }
+    return sum;
+}
+
+int maxAreaOfIsland(int** grid, int gridRowSize, int gridColSize) {
+    int maxArea = 0, tmp;
+    gRow = gridRowSize;
+    gCol = gridColSize;
+    for(int i = 0; i < gridRowSize; ++i){
+        for(int j = 0; j < gridColSize; ++j){
+            if(grid[i][j] == 1){
+                
+                //tmp = DFS(grid, i, j, 0, gridRowSize, gridColSize);
+                tmp = DFS2(grid, i, j, 0);
+                if(tmp > maxArea){
+                    maxArea = tmp;
+                }
+                
+            }
+        }   
+    }
+    return maxArea;
+}
