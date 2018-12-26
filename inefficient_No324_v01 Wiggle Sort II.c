2fbf00147827193271394e24d63115ea16293cd0
worcdlo <37761249+worcdlo@users.noreply.github.com>
2018-12-26 20:58:07 +0800

    Update inefficient_No324_v01 Wiggle Sort II.c

--- inefficient_No324_v01 Wiggle Sort II.c
+++ inefficient_No324_v01 Wiggle Sort II.c
@@ -1,11 +1,70 @@
-void swap(int *a, int *b)
-{
-    int temp = *a;
+void swap(int* a, int* b){
+    int tmp = *a;
     *a = *b;
-    *b = temp;
+    *b = tmp;
 }
 
-void quicksort(int *data, int left, int right)
+void quicksort(int* arr, int left, int right){
+    if(left >= right){
+        return;
+    }
+    
+    int pivot = arr[right];
+    int i = left, j = right - 1;
+    
+    while(i < j){
+        while(arr[i] < pivot && i < j){
+            ++i;
+        }
+        while(arr[j] >= pivot && i < j){
+            --j;
+        }
+        swap(&arr[i], &arr[j]);
+    }
+    
+    if(arr[i] >= pivot){
+        swap(&arr[i], &arr[right]);
+    }else{
+        ++i;
+    }
+    
+    if(i){
+        quicksort(arr, left, i - 1);
+    }
+    quicksort(arr, i + 1, right);
+}
+
+void quicksort2(int* arr, int left, int right){
+    if(left >= right){
+        return;
+    }
+    
+    int pivot = arr[left];
+    int i = left + 1, j = right;
+    
+    while(i < j){
+        while(arr[i] < pivot && i < j){
+            ++i;
+        }
+        while(arr[j] >= pivot && i < j){
+            --j;
+        }
+        swap(&arr[i], &arr[j]);
+    }
+    
+    if(arr[i] >= pivot){
+        swap(&arr[--i], &arr[left]);
+    }else{
+        swap(&arr[i], &arr[left]);
+    }
+    
+    if(i){
+        quicksort2(arr, left, i - 1);
+    }
+    quicksort2(arr, i + 1, right);
+}
+
+void quicksort3(int *data, int left, int right)
 {
     if (left >= right) { 
         return; 
@@ -37,8 +96,8 @@ void quicksort(int *data, int left, int right)
 
     swap(&data[left], &data[j]);
 
-    quicksort(data, left, j - 1);
-    quicksort(data, j + 1, right);
+    quicksort3(data, left, j - 1);
+    quicksort3(data, j + 1, right);
 }
 
 void wiggleSort(int* nums, int numsSize) {
@@ -50,8 +109,9 @@ void wiggleSort(int* nums, int numsSize) {
     int* maxArr = (int*)malloc(sizeof(int) * n2);
     
     //sort
-    quicksort(nums, 0, numsSize - 1);
-
+    quicksort3(nums, 0, numsSize - 1);
+    
+    
     
     for(int i = 0; i < n1; ++i){
         minArr[i] = nums[i];
@@ -71,4 +131,5 @@ void wiggleSort(int* nums, int numsSize) {
         nums[k] = maxArr[i];
         k -= 2;
     }
+    
 }
