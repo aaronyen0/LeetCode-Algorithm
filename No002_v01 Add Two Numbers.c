386e8a3b5885f3dc24b5a3469ac9f1e4dda6ca93
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-06 15:56:56 +0800

    Create No002_v01 Add Two Numbers.c

---
+++ No002_v01 Add Two Numbers.c
@@ -0,0 +1,96 @@
+//寫法類似mergesort的merge操作，但更簡單
+//共分4階段(flag用來判斷需不需要進位)
+//1.若list1和list2都還沒走完時
+//2.list1還有數字但list2走完
+//3.list2還有數字但list1走完
+//4.若最後flag表示要進位，記得插入數字1
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* NewNode(int val){
+    struct ListNode* node = (struct ListNode*)malloc(sizeof(struct ListNode));
+    node->val = val;
+    node->next = NULL;
+    return node;
+}
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    int sum, flag = 0;
+    struct ListNode* crt = NULL, *root;
+    
+    while(l1 && l2){
+        sum = l1->val + l2->val + flag;
+        if(sum > 9){
+            flag = 1;
+            sum -= 10;
+        }else{
+            flag = 0;
+        }
+        
+        if(crt){
+            crt->next = NewNode(sum);
+            crt = crt->next;
+        }else{
+            crt = NewNode(sum);
+            root = crt;
+        }
+        l1 = l1->next;
+        l2 = l2->next;
+    }
+    
+    while(l1){
+        sum = l1->val + flag;
+        if(sum > 9){
+            flag = 1;
+            sum -= 10;
+        }else{
+            flag = 0;
+        }
+        
+        if(crt){
+            crt->next = NewNode(sum);
+            crt = crt->next;
+        }else{
+            crt = NewNode(sum);
+            root = crt;
+        }
+        l1 = l1->next;
+
+    }
+    
+    while(l2){
+        sum = l2->val + flag;
+        if(sum > 9){
+            flag = 1;
+            sum -= 10;
+        }else{
+            flag = 0;
+        }
+        
+        if(crt){
+            crt->next = NewNode(sum);
+            crt = crt->next;
+        }else{
+            crt = NewNode(sum);
+            root = crt;
+        }
+        l2 = l2->next;
+    }
+    
+    if(flag){
+        if(crt){
+            crt->next = NewNode(1);
+        }else{
+            crt = NewNode(1);
+            root = crt;
+        }
+    }
+    return root;
+}
