1e0c728da0294e866e408cc880a5499c0bb0b95b
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-07 11:15:21 +0800

    Update and rename inefficient_No005_v01 Longest Palindromic Substring.c to No005_v02 Longest Palindromic Substring.c

--- No005_v02 Longest Palindromic Substring.c
+++ No005_v02 Longest Palindromic Substring.c
@@ -1,4 +1,47 @@
 /**
+ *參考別人寫法
+ *核心思想跟我的不太一樣
+ *1.先找到跟s[i]重複的字元
+ *2.再從重複的字元之外開始往兩側掃
+ *這樣就沒有所謂的兩個case，大家都是同一個case
+ *另外string長度的計算方法也有改善，顯得更通用(而且對前一版本也適用)
+ *但是依舊只有 beat 89%
+ */
+
+char* longestPalindrome(char* s) {
+    int i, j, k, size, maxLen = 0, tmpLen, maxOffset;
+    char* res;
+    
+    for(size = 0; s[size] != 0; ++size);
+    res = malloc(sizeof(char) * (size + 1));
+
+    for(int i = 0; i < size; ++i){
+        //找到跟s[i]重複的字元
+        k = i;
+        while(k < size && s[i] == s[k]){
+            ++k;
+        }        
+        j = i - 1;
+        
+        //從重複的字元之外開始往兩側掃
+        while(j >= 0 && k < size && s[j] == s[k]){
+            --j;
+            ++k;
+        }
+        tmpLen = k - j - 1;
+        if(tmpLen > maxLen){
+            maxLen = tmpLen;
+            maxOffset = j + 1;
+        }
+    }
+    
+    memcpy(res, &s[maxOffset], maxLen);
+    res[maxLen] = 0;
+    return res;
+}
+
+
+/**
  *O(n^2) beat 77%，個人寫法檢查對稱由中心往兩邊擴張
  *有兩種對稱方式
  *1.長度為奇數的對稱，中心點為一個數字
