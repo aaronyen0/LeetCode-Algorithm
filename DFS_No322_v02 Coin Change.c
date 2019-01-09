29ff6e6f0722f900001550470303d49f237b7ef3
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-09 21:16:02 +0800

    Update DFS_No322_v02 Coin Change.c

--- DFS_No322_v02 Coin Change.c
+++ DFS_No322_v02 Coin Change.c
@@ -1,3 +1,70 @@
+void quicksort3(int *data, int left, int right);
+void swap(int* a, int* b);
+
+//速度超慢 time limit exceeded
+void DFS(int *coins, int i, int remain, int cnt, int *minCnt) {
+	//printf("coins[%d] = %d, remain = %d, cnt = %d\n", i, coins[i], remain, cnt);
+	int num;
+	if (remain == 0) {
+		if (cnt < *minCnt) {
+			*minCnt = cnt;
+		}
+		return;
+	}
+	
+	if (i < 0 || cnt + 2 > *minCnt) {
+		return;
+	}
+	
+	for (int j = remain / coins[i]; j >= 0; --j) {
+		DFS(coins, i - 1, remain - j * coins[i], cnt + j, minCnt);
+	}
+}
+
+//快很多 4ms beat 100%
+void DFS2(int *coins, int i, int remain, int cnt, int *minCnt) {
+	//printf("coins[%d] = %d, remain = %d, cnt = %d\n", i, coins[i], remain, cnt);
+	if (remain == 0) {
+		if (cnt < *minCnt) {
+			*minCnt = cnt;
+		}
+		return;
+	}
+	
+	if (i < 0) {
+		return;
+	}
+	
+	for (int j = remain / coins[i]; j >= 0; --j) {
+		//為何只是把判斷式移到這邊時間卻能差這麼多?
+		//當然call function需要複製引數，需要額外配記憶體...等，確實時間上有差
+		//但從order的角度來看，總覺得不至於會差到超時
+		if (cnt + j + 1 > *minCnt) {
+			break;
+		}
+		DFS2(coins, i - 1, remain - j * coins[i], cnt + j, minCnt);
+	}
+}
+
+int coinChange(int* coins, int coinsSize, int amount) {
+	if (amount == 0) {
+		return 0;
+	}
+
+	int k, res = INT_MAX;
+
+	quicksort3(coins, 0, coinsSize - 1);
+
+	for (k = 0; coins[k] <= amount, k < coinsSize; ++k);
+	--k;
+
+	DFS2(coins, k, amount, 0, &res);
+	if (res != INT_MAX) {
+		return res;
+	}
+	return -1;
+}
+
 void swap(int* a, int* b){
     int tmp = *a;
     *a = *b;
@@ -38,62 +105,4 @@ void quicksort3(int *data, int left, int right)
 
     quicksort3(data, left, j - 1);
     quicksort3(data, j + 1, right);
-}
-
-void DFS(int *coins, int i, int remain, int cnt, int *minStep) {
-	//printf("coins[%d] = %d, remain = %d, cnt = %d\n", i, coins[i], remain, cnt);
-	int num;
-	if (remain == 0 && cnt < *minStep) {
-		*minStep = cnt;
-		return;
-	}
-	
-	if (i < 0 || cnt > *minStep) {
-		return;
-	}
-	
-	for (int j = remain / coins[i]; j >= 0; --j) {
-		DFS(coins, i - 1, remain - j * coins[i], cnt + j, minStep);
-	}
-}
-
-void DFS2(int *coins, int i, int remain, int cnt, int *minStep) {
-	//printf("coins[%d] = %d, remain = %d, cnt = %d\n", i, coins[i], remain, cnt);
-	int num;
-	if (remain == 0) {
-		if (cnt < *minStep) {
-			*minStep = cnt;
-		}
-		return;
-	}
-	
-	if (i < 0 || cnt > *minStep) {
-		return;
-	}
-	
-	for (int j = remain / coins[i]; j >= 0; --j) {
-		if (cnt + j + 1 > *minStep) {
-			break;
-		}
-		DFS2(coins, i - 1, remain - j * coins[i], cnt + j, minStep);
-	}
-}
-
-int coinChange(int* coins, int coinsSize, int amount) {
-	if (amount == 0) {
-		return 0;
-	}
-
-	int k, res, minStep = INT_MAX;
-
-	quicksort3(coins, 0, coinsSize - 1);
-
-	for (k = 0; coins[k] <= amount, k < coinsSize; ++k);
-	--k;
-
-	DFS2(coins, k, amount, 0, &minStep);
-	if (minStep != INT_MAX) {
-		return minStep;
-	}
-	return -1;
 }
