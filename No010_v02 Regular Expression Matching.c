dac66601498aa1983ef8e185b57e10306da68d49
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-11 14:07:45 +0800

    Update and rename No010_v01 Regular Expression Matching.c to No010_v02 Regular Expression Matching.c

--- No010_v02 Regular Expression Matching.c
+++ No010_v02 Regular Expression Matching.c
@@ -1,11 +1,62 @@
 /**
- * 這題一直到繳交好幾次之後，才真正理解題目意思，英文真的要練
+ * 這題一直到繳交好幾次之後，才真正理解題目意思，英文真的要練習
  * 題目說有兩個字串s和p，問s是否跟p等價
- * p字串有機會出現兩個特殊字元：'.'及'*'
- * "."：可以代替任何字元
- * "*"：代表他的前一個字元可以重複n次，n >= 0
+ * p字串有機會出現兩個特殊字元：'.' 及 '*'
+ * '.'：可以代替任何字元
+ * '*'：代表他的前一個字元可以重複n次，n >= 0
  * 例如："a*bc" = "bc" or "abc" or "aabc" or "aaabc" or ...
  */
+
+
+/**
+ * version 2
+ * 發現InRepeating似乎有點多餘
+ * 而且還要多重複判斷'.'
+ * 於是就整合在一起
+ */
+bool isMatch(char* s, char* p) {
+	int sIdx = 0, pIdx = 0, k;
+	char r;
+	while (s[sIdx] && p[pIdx]) {
+		if (p[pIdx + 1] == '*' ) { //若該字元是可變長度的
+			r = p[pIdx];
+            //判斷字元是哪個種類，先剪枝排掉重複的，再進入repeat階段逐一檢查
+			if (r == '.') {
+                do{
+                    pIdx += 2;
+                }while(p[pIdx] != 0 && p[pIdx + 1] == '*');
+                do{
+                    if (isMatch(&s[sIdx], &p[pIdx])) {
+                        return true;
+                    }
+                }while(s[sIdx++] != 0);
+			}else {
+                do{
+                    pIdx += 2;
+                }while(p[pIdx] == r && p[pIdx + 1] == '*');
+                do{
+                    if (isMatch(&s[sIdx], &p[pIdx])) {
+                        return true;
+                    }
+                }while(s[sIdx++] == r);
+			}
+            return false;
+		} else if (s[sIdx] != p[pIdx] && p[pIdx] != '.') { //若該s和p無法配對則離開
+			break;
+		}
+		++sIdx;
+		++pIdx;
+	}
+
+    //若有剩，把多餘的x*丟棄
+    while (p[pIdx] != 0 && p[pIdx + 1] == '*') {
+        pIdx += 2;
+    }
+	
+    //最後發現兩邊都走到底則代表找到解
+	return !s[sIdx] && !p[pIdx];
+}
+
 
 /**
  * version1 , 4ms
