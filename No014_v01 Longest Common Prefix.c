b41ba033de18f2d34768a699bf3e7a6f806fb7bb
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-19 10:23:12 +0800

    Update No014_v01 Longest Common Prefix.c

--- No014_v01 Longest Common Prefix.c
+++ No014_v01 Longest Common Prefix.c
@@ -1,3 +1,26 @@
+//有些題目會要求重配記憶體，但發現這題不用，所以蓋掉原本的陣列即可
+char* longestCommonPrefix(char** strs, int strsSize) {
+    if(strsSize == 0){
+        return "";
+    }
+    int len, j;
+    
+    for(len = 0; strs[0][len] != 0; ++len);
+
+       
+    for(int i = 1; i < strsSize; ++i){
+        for(j = 0; j < len && strs[i][j] != 0 && strs[0][j] == strs[i][j]; ++j);
+        len = j;
+        if(!len){
+            break;
+        }
+    }
+        
+    strs[0][len] = 0;
+    return strs[0];
+}
+
+
 char* longestCommonPrefix(char** strs, int strsSize) {
     if(strsSize == 0){
         return "";
