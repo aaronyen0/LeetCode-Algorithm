de2449bd409d3d1ae8892923cd4cd2a66ee0884d
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-01-19 13:10:28 +0800

    Update No001_v01 Two Sum.c

--- No001_v01 Two Sum.c
+++ No001_v01 Two Sum.c
@@ -1,12 +1,89 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
 
+
+/**
+ * version2 28ms, beats 86%
+ * 寫過sum3後再回來看這題的，發現我一直都有點太依賴hashtable
+ * 雖然這題用hashtable能達100%，但它的限制也很明顯
+ * 以後要盡量想非hashtable的方法
+ */
+void quicksort3(int *data, int *index, int left, int right);
+
+int* twoSum(int* nums, int numsSize, int target) {    
+    int j, k;
+    int *res = (int*)malloc(2 * sizeof(int));
+    int* index = (int*)malloc(numsSize * sizeof(int));
+    for(int i = 0; i < numsSize; ++i){
+        index[i] = i;
+    }
+    quicksort3(nums, index, 0, numsSize - 1);
+    
+    j = 0, k = numsSize - 1;
+    while(j < k){
+        if(nums[j] + nums[k] < target){
+            ++j;
+            continue;
+        }
+        if(nums[j] + nums[k] > target){
+            --k;
+            continue;
+        }
+        res[0] = index[j];
+        res[1] = index[k];
+        break;
+    }
+    return res;
+}
+
+void swap(int* a, int* b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void quicksort3(int *data, int *index, int left, int right)
+{
+    if (left >= right) { 
+        return; 
+    }
+    int pivot = data[left];
+    int i = left + 1;
+    int j = right;
+
+    while (1){
+        while (i <= right){
+            if (data[i] >= pivot){
+                break;
+            }
+            ++i;
+        }
+
+        while (j > left){
+            if (data[j] < pivot){
+                break;
+            }
+            --j;
+        }
+
+        if (i > j) { 
+            break; 
+        }
+        swap(&data[i], &data[j]);
+        swap(&index[i], &index[j]);
+    }
+
+    swap(&data[left], &data[j]);
+    swap(&index[left], &index[j]);
+
+    quicksort3(data, index, left, j - 1);
+    quicksort3(data, index, j + 1, right);
+}
+
+//version1
 int* twoSum(int* nums, int numsSize, int target) {
     int* res = (int*)malloc(sizeof(int) * 2);
     for(int i = 0; i < numsSize; ++i){
         for(int j = i + 1; j < numsSize; ++j){
-            if(nums[i] + nums[j] == target){
+            if(nums[i] + nums[j] == target){//應該要改成檢查nums[j] == dis，其中dis = target - nums[i]才不會一直重算這一步
                 res[0] = i;
                 res[1] = j;
                 return res;
