096ebb665173d9b8628034adbe39a78ee6d4cf21
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-06-02 23:18:40 +0800

    Update inefficient DP NoA48_v01 Longest String Chain.c

--- inefficient DP NoA48_v01 Longest String Chain.c
+++ inefficient DP NoA48_v01 Longest String Chain.c
@@ -8,6 +8,91 @@
 
 
 /**
+ * version2, 16ms, beats 95%
+ * 剪枝
+ * 由於我的排序是counting sort，而且過程中設計一個陣列專門用來存offset
+ * 所以根本就知道長度-1的字串的起訖點
+ * 可以減少很多if判斷
+ */
+ 
+int CheckPredecessor(char* word1, char* word2);
+
+int longestStrChain(char ** words, int wordsSize){
+    int k, q, tmpMax, tmpLen;
+    int res = 1;
+    int counting[17] = {0};
+    int offset[17] = {0};
+    int* dp = (int*)malloc(wordsSize * sizeof(int));
+    char** sortWords = (char**)malloc(wordsSize * sizeof(char*));
+    int* wordLen = (int*)malloc(wordsSize * sizeof(int));
+    //int* sortLen = (int*)malloc(wordsSize * sizeof(int));
+    
+    //counting sort
+    for(int i = 0; i < wordsSize; ++i){
+        wordLen[i] = strlen(words[i]);
+        counting[wordLen[i]]++;
+    }
+    for(int i = 2; i < 17; ++i){
+        offset[i] = offset[i - 1] + counting[i - 1];
+    }
+    
+    for(int i = 0; i < wordsSize; ++i){
+        sortWords[offset[wordLen[i]]++] = words[i];
+        //sortLen[offset[wordLen[i]]++] = wordLen[i];
+    }
+    k = 1;
+    q = 0;
+    while(k < 17){
+        for(int i = 0; i < counting[k]; ++q, ++i){
+            wordLen[q] = k;
+        }
+        ++k;
+    }
+
+    //類似最大子陣列寫法
+    for(int i = 0; i < wordsSize; ++i){
+        if(wordLen[i] == 1){
+           dp[i] = 1;
+            continue;
+        }
+        
+        tmpMax = 1;
+        tmpLen = wordLen[i];
+        for(int j = offset[tmpLen - 2]; j < offset[tmpLen - 1]; ++j){ //剪枝
+            if(CheckPredecessor(sortWords[j], sortWords[i]) == -1){
+                continue;
+            }
+            if(dp[j] >= tmpMax){
+                tmpMax = dp[j] + 1;
+            }
+        }
+        if(tmpMax > res){
+            res = tmpMax;
+        }
+        dp[i] = tmpMax;
+    }
+    
+    
+    return res;
+}
+
+int CheckPredecessor(char* word1, char* word2){
+    int l = 0, r = 0, diff = 0;
+    while(*word1){
+        if(*word1 != *word2){
+            word2++;
+            if(++diff > 1){
+                return -1;
+            }
+            continue;
+        }
+        word1++;
+        word2++;
+    }
+    return 1;
+}
+
+/**
  * version1, 28ms, beats 86%
  * 1. 依照word的長度重排字串
  * 2. 定義dp[i]：為終點為字串i時最長的predecessor集合
