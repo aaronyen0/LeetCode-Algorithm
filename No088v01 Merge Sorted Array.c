b975bf44f6578e71563de83fc057c461febe9fe2
worcdlo <37761249+worcdlo@users.noreply.github.com>
2019-08-03 10:50:49 +0800

    Update No088v01 Merge Sorted Array.c

--- No088v01 Merge Sorted Array.c
+++ No088v01 Merge Sorted Array.c
@@ -6,6 +6,31 @@
  */
  
 /**
+ * version2, 4ms, 82%
+ * 參考別人的寫法
+ * 值從最後寫回來
+ * 可以節省一些空間和迴圈:
+ * 1. 不用額外配置記憶體
+ * 2. 出第一個while後不用再額外跑nums1迴圈
+ */
+
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
+    int i = m - 1, j = n - 1, offset = m + n - 1;
+
+    while(i >= 0 && j >= 0){
+        if(nums1[i] > nums2[j]){
+            nums1[offset--] = nums1[i--];
+        }else{
+            nums1[offset--] = nums2[j--];
+        }
+    }
+    while(j >= 0){
+        nums1[offset--] = nums2[j--];
+    }
+}
+
+
+/**
  * version1, 4ms, 82%
  * merge sort的後半段
  * 用另一個buffer去接merge的結果
